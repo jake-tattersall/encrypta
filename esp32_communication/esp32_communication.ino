ad8a8386798ad938d2009e00c79593cb4a593382
AustinVosler <austinpvosler@gmail.com>
2024-10-05 02:59:26 -0400

    Update esp32_communication.ino

--- esp32_communication/esp32_communication.ino
+++ esp32_communication/esp32_communication.ino
@@ -1,79 +1,146 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_Keypad.h>
 
-Adafruit_Keypad keypad;
+#include <esp_now.h>
+
+#define KEYPAD_PID1824
+#define MAXCHARS 256
+
+typedef struct Msg {
+  char[256] chars;
+  int len;
+} Msg;
+
+Msg msg;
+
+
+char alphabet[] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 
+                  'U', 'V', 'W', 'X', 'Y', 'Q', 'Z'};
 
 byte characterTable[] = { 1, 2, 3,
                           4, 5, 6,
                           7, 8, 9,
-                          10, 11, 12 }
+                          10, 11, 12 };
 
 //               row1   2     3     4
-byte rowPin[] = {null, null, null, null};
+byte rowPin[] = {NULL, NULL, NULL, NULL};
 //               col1   2     3
-byte colPin[] = {null, null, null};
+byte colPin[] = {NULL, NULL, NULL};
+
+Adafruit_Keypad keypad(characterTable, rowPin, colPin, 4, 3);
 
 int timesPressed = 0;
+char toPush;
 
-enum inputMode { CHAR, DIGIT };
+enum inputMode { CHAR, DIGIT, RECV };
+inputMode currentMode = RECV;
 
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  keypadSetup();
+  msg.len = 0;
+  toPush = '\0';
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello World!");
-  tick()
+  keypad.tick();
 
-  keypadEvent e = keypad.read()
-  if (e != null)
-    readInput(e);
-}
-
-void keypadSetup(byte[] table)
-{
-  keypad = new Adafruit_Keypad(characterTable, rowPin, colPin, 4, 3);
+  Serial.println(keypad.available());
+  if (keypad.available() != NULL)
+    readInput(keypad.read());
 }
 
 void readInput(keypadEvent e)
 {
-  switch (inputMode)
-    case "CHAR":
-      readChar();
+  switch (currentMode)
+  {
+    case CHAR:
+      readChar(e);
       break;
-    case "DIGIT":
-      readDigit();
+    case DIGIT:
+      readDigit(e);
       break;
+  }
 }
 
 void readChar(keypadEvent e)
 {
-  int key = e.KEY;
+  int key = e.bit.KEY;
+  
+  if(checkSymbols(key))
+    return;
 
+  timesPressed++;
 
+  int mod = timesPressed % 3;
+  if (mod == 0)
+    mod = 3;
+
+  int indx = ((key - 2) * 3) + mod;
+  toPush = alphabet[indx];
 }
 
 void readDigit(keypadEvent e)
 {
+  int key = e.bit.KEY;
 
+  if (checkSymbols(key))
+    return;
+  
+  if (key == 11)
+    key = 0;
+}
+
+bool checkSymbols(int key)
+{
+  if (key == 10)
+  {
+    space();
+    return true;
+  }
+    
+  if (key == 12)
+  {
+    confirm();
+    return true;
+  }
+
+  return false;
+}
+
+void space()
+{
+  toPush = ' ';
+  pushToDisplay();
+}
+
+void confirm()
+{
+  clearBuffer();
 }
 
 void clearBuffer()
 {
-  // if commit character button
-  // keypad.clear();
-  // timesPressed = 0;
+  keypad.clear();
+  timesPressed = 0;
+}
 
-  // if backspace button
-  // keypad.clear();
-  // timesPressed = 0;
+void pushToDisplay()
+{
+  msg.chars[msg.len++] = toPush;
+  toPush = '\0';
 
-  // if cancel button
-  // keypad.clear();
-  // timesPressed = 0;
+  // Update OLED
+}
+
+void deleteFromDisplay() 
+{
+  if (msg.len <= 0)
+    return;
+  
+  msg.len--;
+
+  // Update OLED
 }
