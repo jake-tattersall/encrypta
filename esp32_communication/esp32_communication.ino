588a1f23ad2cdb1cf82c06ad479bc8a4b7eaa59e
AustinVosler <austinpvosler@gmail.com>
2024-10-05 00:34:51 -0400

    keypad input boilerplate

--- esp32_communication/esp32_communication.ino
+++ esp32_communication/esp32_communication.ino
@@ -1,12 +1,70 @@
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Keypad.h>
 
+Adafruit_Keypad keypad;
 
+byte characterTable[] = { 1, 2, 3,
+                          4, 5, 6,
+                          7, 8, 9,
+                          10, 11, 12 }
 
-void setup() {
+//               row1   2     3     4
+byte rowPin[] = {null, null, null, null};
+//               col1   2     3
+byte colPin[] = {null, null, null};
+
+enum inputMode { CHAR, DIGIT };
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
+  keypadSetup();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   Serial.println("Hello World!");
+  tick()
+}
+
+void keypadSetup(byte[] table)
+{
+  keypad = new Adafruit_Keypad(characterTable, rowPin, colPin, 4, 3);
+}
+
+void readInput()
+{
+  
+
+  switch (inputMode)
+    case "CHAR":
+      readChar();
+      break;
+    case "DIGIT":
+      readDigit();
+      break;
+}
+
+void readChar()
+{
+
+}
+
+void readDigit()
+{
+
+}
+
+void clearBuffer()
+{
+  // if commit character button
+  // keypad.clear();
+
+  // if backspace button
+  // keypad.clear();
+
+  // if cancel button
+  // keypad.clear();
 }
