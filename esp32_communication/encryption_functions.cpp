11fc587303f48fd47e47cbcecb47b9649bcc16f1
AustinVosler <austinpvosler@gmail.com>
2024-10-06 01:52:53 -0400

    formatting

--- esp32_communication/encryption_functions.cpp
+++ esp32_communication/encryption_functions.cpp
@@ -19,21 +19,22 @@ void toLowerCase(char plain[], int ps)
   }
 }
 
-int removeSpaces(char* plain, int ps)
-{
-  int i, count = 0;
-  for(i = 0; i < ps; i++)
-  {
-    if(plain[i] != ' ')
-    {
-      plain[count++] = plain[i];
-    }
-    plain[count] = '\0';
-    return count;
-  }
+// int removeSpaces(char* plain, int ps)
+// {
+//   int i, count = 0;
+//   for(i = 0; i < ps; i++)
+//   {
+//     if(plain[i] != ' ')
+//     {
+//       plain[count++] = plain[i];
+//     }
+//     plain[count] = '\0';
+//     return count;
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
+
 // Generating a 5x5 square for Playfair cipher, ks is key length
 void generateKeyTable(char key[], int ks, char keyT[5][5])
 {
@@ -242,57 +243,10 @@ char* encryptByPlayfair(char str[], char key[])
 
   return newStr;
 }
+
 /*
-
   Decryption of Playfair
-
 */
-void keyTableDecrypt(char key[], int ks, char keyT[5][5]){
-
-  int i, j, k, flag = 0, *dicty;
-
-  dicty = (int*)calloc(26, sizeof(int));
-
-  for(i = 0; i < ks; i++){
-    if(key[i] != 'j'){
-      dicty[key[i] - 97] = 2;
-    }
-  }
-  dicty['j' - 97] = 1;
-
-  i = 0;
-  j = 0;
-  for(k = 0; k < ks; k++){
-    if (dicty[key[k] - 97] == 2) {
-        dicty[key[k] - 97] -= 1;
-        keyT[i][j] = key[k];
-        j++;
-        if (j == 5) {
-          i++;
-          j = 0;
-       }
-    }
-  }
-
-  for (k = 0; k < 26; k++) {
-    if (dicty[k] == 0) {
-      keyT[i][j] = (char)(k + 97);
-      j++;
-      if (j == 5) {
-        i++;
-        j = 0;
-      }
-    }
-  }
-  /*
-  for(int t = 0; t < 5; t++){
-    for(int m = 0; m < 5; m++){
-      Serial.print(keyT[t][m]);
-    }
-    Serial.println();
-  }
-  */
-}
 
 int overloadmod5(int a){
   if(a < 0){
@@ -387,5 +341,4 @@ char* decryptPlayfair(char str[], char key[])
   printf("\n");
 
   return str;
-
 }
